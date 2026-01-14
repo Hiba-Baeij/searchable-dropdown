1aacb089601bcab96758a008ff853dab7d574147
Hiba-Baeij <Hibabeiaj@gmail.com>
2026-01-14 13:56:54 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,32 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           'mantine-core': ['@mantine/core'],
           'mantine-hooks': ['@mantine/hooks'],
-        },
+        }
       },
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true,
     },
   },
   optimizeDeps: {
-    include: ['@mantine/core', '@mantine/hooks'],
+    include: ['react', 'react-dom', 'react/jsx-runtime', '@mantine/core', '@mantine/hooks'],
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom'],
   },
 });
